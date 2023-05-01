16c57dac9cef0a0dc0bce2fc523c1bc08b8d8c18
Bissonhoe <paulovictorbissonho@gmail.comm>
2023-05-01 02:06:17 -0300

    add THREE.js

--- build/index.js
+++ build/index.js
@@ -1,9 +1,35 @@
 "use strict";
-// Define the Entity class
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var THREE = __importStar(require("three"));
+// Define a classe Entity
 var Entity = /** @class */ (function () {
     function Entity(id) {
         this.id = id;
         this.components = [];
+        this.object3D = new THREE.Object3D();
     }
     Entity.prototype.addComponent = function (component) {
         this.components.push(component);
@@ -16,17 +42,19 @@ var Entity = /** @class */ (function () {
     };
     return Entity;
 }());
-// Define the components
+// Define os componentes
 var PositionComponent = /** @class */ (function () {
-    function PositionComponent(x, y) {
+    function PositionComponent(x, y, z) {
         this.x = x;
         this.y = y;
+        this.z = z;
     }
     return PositionComponent;
 }());
 var RenderComponent = /** @class */ (function () {
-    function RenderComponent(sprite) {
-        this.sprite = sprite;
+    function RenderComponent(geometry, material) {
+        this.geometry = geometry;
+        this.material = material;
     }
     return RenderComponent;
 }());
@@ -36,55 +64,94 @@ var RotationComponent = /** @class */ (function () {
     }
     return RotationComponent;
 }());
-// Define the systems
 var RenderSystem = /** @class */ (function () {
-    function RenderSystem(entities, frameRate) {
-        this.entities = entities;
-        this.frameRate = frameRate;
-        this.intervalId = 0;
-    }
-    RenderSystem.prototype.start = function () {
+    function RenderSystem(entities, container) {
         var _this = this;
-        var intervalTime = 1000 / this.frameRate;
-        this.intervalId = setInterval(function () { return _this.update(); }, intervalTime);
-    };
-    RenderSystem.prototype.stop = function () {
-        clearInterval(this.intervalId);
-    };
-    RenderSystem.prototype.update = function () {
-        for (var _i = 0, _a = this.entities; _i < _a.length; _i++) {
-            var entity = _a[_i];
-            var positionComponent = entity.getComponent('PositionComponent');
+        this.entities = entities;
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
+        this.renderer = new THREE.WebGLRenderer({ antialias: true });
+        this.renderer.setSize(container.clientWidth, container.clientHeight);
+        container.appendChild(this.renderer.domElement);
+        this.camera.position.z = 5;
+        this.entities.forEach(function (entity) {
             var renderComponent = entity.getComponent('RenderComponent');
+            var positionComponent = entity.getComponent('PositionComponent');
             var rotationComponent = entity.getComponent('RotationComponent');
-            if (positionComponent && renderComponent && rotationComponent) {
-                var x = positionComponent.x, y = positionComponent.y;
-                var sprite = renderComponent.sprite;
+            if (renderComponent && positionComponent && rotationComponent) {
+                var geometry = renderComponent.geometry, material = renderComponent.material;
+                var x = positionComponent.x, y = positionComponent.y, z = positionComponent.z;
                 var rotation = rotationComponent.rotation;
-                console.log("Rendering entity ".concat(entity.id, " at position (").concat(x, ", ").concat(y, ") with sprite ").concat(sprite, " and rotation ").concat(rotation));
+                var mesh = new THREE.Mesh(geometry, material);
+                mesh.position.set(x, y, z);
+                mesh.rotation.copy(rotation);
+                entity.object3D = mesh;
+                _this.scene.add(mesh);
             }
-        }
+        });
+    }
+    RenderSystem.prototype.update = function (deltaTime) {
+        this.entities.forEach(function (entity) {
+            var positionComponent = entity.getComponent('PositionComponent');
+            var rotationComponent = entity.getComponent('RotationComponent');
+            if (positionComponent && rotationComponent) {
+                var x = positionComponent.x, y = positionComponent.y, z = positionComponent.z;
+                var rotation = rotationComponent.rotation;
+                entity.object3D.position.set(x, y, z);
+                entity.object3D.rotation.copy(rotation);
+            }
+        });
+        this.renderer.render(this.scene, this.camera);
     };
     return RenderSystem;
 }());
-// Usage of the engine
+// Loop do jogo com requestAnimationFrame
+var Game = /** @class */ (function () {
+    function Game(frameRate, container) {
+        this.frameRate = frameRate;
+        this.deltaTime = 1000 / frameRate;
+        this.lastFrameTime = 0;
+        this.running = false;
+        this.renderSystem = new RenderSystem(entities, container);
+    }
+    Game.prototype.start = function () {
+        this.running = true;
+        this.lastFrameTime = performance.now();
+        this.loop();
+    };
+    Game.prototype.stop = function () {
+        this.running = false;
+    };
+    Game.prototype.loop = function () {
+        var _this = this;
+        if (!this.running)
+            return;
+        var currentTime = performance.now();
+        var elapsed = currentTime - this.lastFrameTime;
+        if (elapsed >= this.deltaTime) {
+            this.renderSystem.update(elapsed);
+            this.lastFrameTime = currentTime;
+        }
+        requestAnimationFrame(function () { return _this.loop(); });
+    };
+    return Game;
+}());
+// Uso da engine
 var entities = [];
-// Create entities
+// Criar entidades
 var entity1 = new Entity(1);
-entity1.addComponent(new PositionComponent(10, 20));
-entity1.addComponent(new RenderComponent('sprite1.png'));
-entity1.addComponent(new RotationComponent(0));
+entity1.addComponent(new PositionComponent(0, 0, 0));
+entity1.addComponent(new RenderComponent(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0xff0000 })));
+entity1.addComponent(new RotationComponent(new THREE.Euler(0, 0, 0)));
 entities.push(entity1);
 var entity2 = new Entity(2);
-entity2.addComponent(new PositionComponent(30, 40));
-entity2.addComponent(new RenderComponent('sprite2.png'));
-entity2.addComponent(new RotationComponent(45));
+entity2.addComponent(new PositionComponent(2, 0, 0));
+entity2.addComponent(new RenderComponent(new THREE.BoxGeometry(0.5, 32, 32), new THREE.MeshBasicMaterial({ color: 0x00ff00 })));
+entity2.addComponent(new RotationComponent(new THREE.Euler(0, Math.PI / 2, 0)));
 entities.push(entity2);
-// Create the render system
-var renderSystem = new RenderSystem(entities, 60);
-// Start the render system
-renderSystem.start();
-// Stop the render system after 5 seconds
-setTimeout(function () {
-    renderSystem.stop();
-}, 5000);
+// Criação do container para renderização
+var container = document.createElement('div');
+document.body.appendChild(container);
+// Inicialização do jogo
+var game = new Game(60, container);
+game.start();
--- src/index.tsx
+++ src/index.tsx
@@ -1,125 +1,187 @@
-// Define the Component interface
+import * as THREE from 'three';
+
+
+// Define a interface Component
 interface Component {
-    // Define the necessary fields for each component
+  // Define os campos necessários para cada componente
+}
+
+// Define a classe Entity
+class Entity {
+  id: number;
+  components: Component[];
+  object3D: THREE.Object3D;
+
+  constructor(id: number) {
+    this.id = id;
+    this.components = [];
+    this.object3D = new THREE.Object3D();
   }
-  
-  // Define the Entity class
-  class Entity {
-    id: number;
-    components: Component[];
-  
-    constructor(id: number) {
-      this.id = id;
-      this.components = [];
-    }
-  
-    addComponent(component: Component) {
-      this.components.push(component);
-    }
-  
-    removeComponent(componentType: string) {
-      this.components = this.components.filter(
-        (component) => component.constructor.name !== componentType
-      );
-    }
-  
-    getComponent(componentType: string) {
-      return this.components.find(
-        (component) => component.constructor.name === componentType
-      );
-    }
+
+  addComponent(component: Component) {
+    this.components.push(component);
   }
-  
-  // Define the components
-  class PositionComponent implements Component {
-    x: number;
-    y: number;
-  
-    constructor(x: number, y: number) {
-      this.x = x;
-      this.y = y;
-    }
+
+  removeComponent(componentType: string) {
+    this.components = this.components.filter(
+      (component) => component.constructor.name !== componentType
+    );
   }
-  
-  class RenderComponent implements Component {
-    sprite: string;
-  
-    constructor(sprite: string) {
-      this.sprite = sprite;
-    }
+
+  getComponent(componentType: string) {
+    return this.components.find(
+      (component) => component.constructor.name === componentType
+    );
   }
-  
-  class RotationComponent implements Component {
-    rotation: number;
-  
-    constructor(rotation: number) {
-      this.rotation = rotation;
-    }
+}
+
+// Define os componentes
+class PositionComponent implements Component {
+  x: number;
+  y: number;
+  z: number;
+
+  constructor(x: number, y: number, z: number) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
   }
-  
-  // Define the systems
-  class RenderSystem {
-    entities: Entity[];
-    frameRate: number;
-    intervalId: number;
-  
-    constructor(entities: Entity[], frameRate: number) {
-      this.entities = entities;
-      this.frameRate = frameRate;
-      this.intervalId = 0;
-    }
-  
-    start() {
-      const intervalTime = 1000 / this.frameRate;
-      this.intervalId = setInterval(() => this.update(), intervalTime);
-    }
-  
-    stop() {
-      clearInterval(this.intervalId);
-    }
-  
-    update() {
-      for (const entity of this.entities) {
-        const positionComponent = entity.getComponent('PositionComponent') as PositionComponent;
-        const renderComponent = entity.getComponent('RenderComponent') as RenderComponent;
-        const rotationComponent = entity.getComponent('RotationComponent') as RotationComponent;
-  
-        if (positionComponent && renderComponent && rotationComponent) {
-          const { x, y } = positionComponent;
-          const { sprite } = renderComponent;
-          const { rotation } = rotationComponent;
-  
-          console.log(
-            `Rendering entity ${entity.id} at position (${x}, ${y}) with sprite ${sprite} and rotation ${rotation}`
-          );
-        }
+}
+
+class RenderComponent implements Component {
+  geometry: THREE.BoxGeometry;
+  material: THREE.Material;
+
+  constructor(geometry: THREE.BoxGeometry, material: THREE.Material) {
+    this.geometry = geometry;
+    this.material = material;
+  }
+}
+
+class RotationComponent implements Component {
+  rotation: THREE.Euler;
+
+  constructor(rotation: THREE.Euler) {
+    this.rotation = rotation;
+  }
+}
+
+class RenderSystem {
+  entities: Entity[];
+  scene: THREE.Scene;
+  camera: THREE.PerspectiveCamera;
+  renderer: THREE.WebGLRenderer;
+
+  constructor(entities: Entity[], container: HTMLElement) {
+    this.entities = entities;
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+
+    this.renderer.setSize(container.clientWidth, container.clientHeight);
+    container.appendChild(this.renderer.domElement);
+
+    this.camera.position.z = 5;
+
+    this.entities.forEach((entity) => {
+      const renderComponent = entity.getComponent('RenderComponent') as RenderComponent;
+      const positionComponent = entity.getComponent('PositionComponent') as PositionComponent;
+      const rotationComponent = entity.getComponent('RotationComponent') as RotationComponent;
+
+      if (renderComponent && positionComponent && rotationComponent) {
+        const { geometry, material } = renderComponent;
+        const { x, y, z } = positionComponent;
+        const { rotation } = rotationComponent;
+
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.position.set(x, y, z);
+        mesh.rotation.copy(rotation);
+
+        entity.object3D = mesh;
+        this.scene.add(mesh);
       }
-    }
+    });
   }
-  
-  // Usage of the engine
-  const entities: Entity[] = [];
-  
-  // Create entities
-  const entity1 = new Entity(1);
-  entity1.addComponent(new PositionComponent(10, 20));
-  entity1.addComponent(new RenderComponent('sprite1.png'));
-  entity1.addComponent(new RotationComponent(0));
-  entities.push(entity1);
-  
-  const entity2 = new Entity(2);
-  entity2.addComponent(new PositionComponent(30, 40));
-  entity2.addComponent(new RenderComponent('sprite2.png'));
-  entity2.addComponent(new RotationComponent(45));
-  entities.push(entity2);
-  
-  // Create the render system
-  const renderSystem = new RenderSystem(entities, 60);
-  
-  // Start the render system
-  renderSystem.start();
-  
-  // Stop the render system after 5 seconds
-  setTimeout(() => {
-    renderSystem.stop();
-  }, 5000);
+
+  update(deltaTime: number) {
+    this.entities.forEach((entity) => {
+      const positionComponent = entity.getComponent('PositionComponent') as PositionComponent;
+      const rotationComponent = entity.getComponent('RotationComponent') as RotationComponent;
+
+      if (positionComponent && rotationComponent) {
+        const { x, y, z } = positionComponent;
+        const { rotation } = rotationComponent;
+
+        entity.object3D.position.set(x, y, z);
+        entity.object3D.rotation.copy(rotation);
+      }
+    });
+
+    this.renderer.render(this.scene, this.camera);
+  }
+}
+
+// Loop do jogo com requestAnimationFrame
+class Game {
+  frameRate: number;
+  deltaTime: number;
+  lastFrameTime: number;
+  running: boolean;
+  renderSystem: RenderSystem;
+
+  constructor(frameRate: number, container: HTMLElement) {
+    this.frameRate = frameRate;
+    this.deltaTime = 1000 / frameRate;
+    this.lastFrameTime = 0;
+    this.running = false;
+    this.renderSystem = new RenderSystem(entities, container);
+  }
+
+  start() {
+    this.running = true;
+    this.lastFrameTime = performance.now();
+    this.loop();
+  }
+
+  stop() {
+    this.running = false;
+  }
+
+  loop() {
+    if (!this.running) return;
+
+    const currentTime = performance.now();
+    const elapsed = currentTime - this.lastFrameTime;
+
+    if (elapsed >= this.deltaTime) {
+      this.renderSystem.update(elapsed);
+      this.lastFrameTime = currentTime;
+    }
+
+    requestAnimationFrame(() => this.loop());
+  }
+}
+
+// Uso da engine
+const entities: Entity[] = [];
+
+// Criar entidades
+const entity1 = new Entity(1);
+entity1.addComponent(new PositionComponent(0, 0, 0));
+entity1.addComponent(new RenderComponent(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0xff0000 })));
+entity1.addComponent(new RotationComponent(new THREE.Euler(0, 0, 0)));
+entities.push(entity1);
+
+const entity2 = new Entity(2);
+entity2.addComponent(new PositionComponent(2, 0, 0));
+entity2.addComponent(new RenderComponent(new THREE.BoxGeometry(0.5, 32, 32), new THREE.MeshBasicMaterial({ color: 0x00ff00 })));
+entity2.addComponent(new RotationComponent(new THREE.Euler(0, Math.PI / 2, 0)));
+entities.push(entity2);
+
+// Criação do container para renderização
+const container = document.createElement('div');
+document.body.appendChild(container);
+
+// Inicialização do jogo
+const game = new Game(60, container);
+game.start();
